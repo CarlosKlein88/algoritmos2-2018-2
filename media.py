90b70e642604e0a4e0397e91f8a4f816a320eab6
Carlos Eugênio Klein <carinti@gmail.com>
2018-08-22 21:31:37 -0300

    adicionado metodo maior numero

--- media.py
+++ media.py
@@ -2,4 +2,17 @@ v1 = 4
 v2 = 2
 media = (v1+v2)/2
 
-print (media)
+print(media)
+
+v3 = 8
+v4 = 9
+v5 = 7
+maior = 0
+
+if v3 > v4 and v3 > v5:
+    maior = v3
+elif v4 > v3 and v4 > v5:
+    maior = v4
+else:
+    maior = v5
+print("O maior numero é: ", maior)
